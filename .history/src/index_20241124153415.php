438aac910fb1514d7a5e91a99f5c9d14f88be9f9
sadangel1 <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>
2024-11-24 19:25:12 +0700

    first commit

---
+++ .history/src/index_20241124153415.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Docker Apache</title>
+</head>
+<body>
+    <h1>Welcome to Docker-powered Apache Web Server!</h1>
+    <p>Your web application is running inside a Docker container with Apache.</p>
+</body>
+</html>
---
+++ .history/src/index_20241124192025.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "db"; // hoặc tên máy chủ của MySQL
+$username = "root";        // Tên người dùng MySQL
+$password = "123456"; // Mật khẩu MySQL
+$dbname = "demo1";  // Tên cơ sở dữ liệu
+FD
+// Tạo kết nối
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if (!$conn) {
+    die("Kết nối thất bại: " . mysqli_connect_error());
+} else {
+    echo "Kết nối MySQL thành công!";
+}
+
+// Đóng kết nối
+mysqli_close($conn);
+?>
